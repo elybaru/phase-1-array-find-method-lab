a68ab4d7e0671bbb9bd88f351746b9bac52595b5
Ely Karr <elykarr@gmail.com>
2021-09-28 11:53:39 -0700

    commit tree b4237db77f1b1664f89031c11fff0a6446718782

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(array) {
+    const result = array.find(record => record.result === "W")
+    if (result) {
+        return result.year 
+    } else {
+        return undefined
+    }
+    
+}
